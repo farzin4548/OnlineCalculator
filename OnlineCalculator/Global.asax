0e5f2d6d2929f0bc7cbb097ff0220962fd055a6a
farzin4548 <farzin4548@gmail.com>
2017-12-23 17:02:02 +0330

    init master

---
+++ OnlineCalculator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineCalculator.MvcApplication" Language="C#" %>
---
+++ OnlineCalculator/Models/Expression.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineCalculator.Models
+{
+    public class Expression
+    {
+        public float Operand1 { get; set; }
+        public float Operand2 { get; set; }
+        public Operator Operator { get; set; }
+        //public float Result
+        //{
+        //    get
+        //    {
+        //        switch (Operator)
+        //        {
+        //            case Operator.SUBTRACTION:
+        //                return Operand1 - Operand2;
+        //                break;
+        //            case Operator.ADD:
+        //                break;
+        //            case Operator.DEVIDE:
+        //                break;
+        //            case Operator.MULTIPLY:
+        //                break;
+        //            default:
+        //                break;
+        //        }
+        //    }
+        //}
+
+                                           //public float Result { get {
+                                           //        switch(this.Operator)
+                                           //           // switch (switch_on)
+                                           //        {
+                                           //            case Operator.ADD:
+                                           //                Operand1 + Operand2;
+
+
+        //            default:
+        //        }
+
+        //    } set; }
+        //public 
+    }
+
+
+    public enum Operator
+    {
+        SUBTRACTION, ADD, DEVIDE, MULTIPLY
+    }
+}
