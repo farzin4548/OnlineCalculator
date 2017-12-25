733de07a22d39a716c81ff0a2d1a978b7637bbcf
farzin4548 <farzin4548@gmail.com>
2017-12-25 11:50:31 +0330

    add TestFixtures

---
+++ CalcUnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace CalcUnitTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test()]
+        public void UnitTest2()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+        
+    }
+}
