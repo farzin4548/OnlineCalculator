4bb127e0200faf3684c07e17137a2ce6a78555eb
farzin4548 <farzin4548@gmail.com>
2017-12-25 11:26:24 +0330

    test page2

---
+++ OnlineCalculator/Models/IUSDGBPExchangeRateFeed.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCalculator.Models
+{
+    public interface IUSDGBPExchangeRateFeed
+    {
+        int GetGBPRate();
+        int GetGBPRate2();
+        int GetGBPRate3();
+        int GetGBPRate4();
+        int GetGBPRate5();
+        int GetGBPRate6();
+        int GetGBPRate7();
+        int GetGBPRate8();
+        int GetGBPRate9();
+        int GetGBPRate10();
+        int GetGBPRate11();
+        int GetGBPRate12();
+    }
+
+    public class USDGBPExchangeRateFeed : IUSDGBPExchangeRateFeed
+    {
+        public int GetGBPRate() { return 0; }
+
+        public int GetGBPRate10()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGBPRate11()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGBPRate12()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGBPRate2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGBPRate3()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGBPRate4()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGBPRate5()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGBPRate6()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGBPRate7()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGBPRate8()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGBPRate9()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OnlineCalculator/Models/StringCalculation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineCalculator.Models
+{
+    public class StringCalculation
+    {
+        public int Add(string expr)
+        {
+            //if(expr == string.Empty) { return 0; }
+            if (string.IsNullOrWhiteSpace(expr)) return 0;
+
+            var splitedExpr = expr.Split(new char[] { ',', '\n' });
+            if (splitedExpr.Length > 2 || splitedExpr.Length < 2)
+                throw new ArgumentException();
+            if((splitedExpr[0]).Contains("-") ||(splitedExpr[1]).Contains("-"))
+                throw new ArgumentException();
+
+            return int.Parse(splitedExpr[0]) + int.Parse(splitedExpr[1]);
+        }
+    }
+}
